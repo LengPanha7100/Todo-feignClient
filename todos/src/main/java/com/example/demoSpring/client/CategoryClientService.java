5f28b05af752beb399864d713e32685221bc91c6
LengPanha7100 <160731555+LengPanha7100@users.noreply.github.com>
2024-12-11 23:16:36 +0700

    update

--- todos/src/main/java/com/example/demoSpring/client/CategoryClientService.java
+++ todos/src/main/java/com/example/demoSpring/client/CategoryClientService.java
@@ -2,15 +2,12 @@ package com.example.demoSpring.client;
 
 import com.example.demoSpring.model.APIResponse;
 import com.example.demoSpring.model.Category;
-import jdk.dynalink.linker.LinkerServices;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.List;
-
-@FeignClient(name = "category-service" , url = "http://localhost:8888/api/v1/category")
+@FeignClient(name = "category-service" , url = "http://category-api:8081/api/v1/category")
 public interface CategoryClientService {
     @GetMapping("/{id}")
     ResponseEntity<APIResponse<Category>> getCategoryById(@PathVariable Long id);
